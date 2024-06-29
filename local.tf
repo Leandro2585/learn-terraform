b17577bb44ab725ec98f7893a27ea52f5b42e133
Leandro Real <leo.real2585@gmail.com>
2024-06-29 00:08:39 -0300

    feat: init TF

---
+++ local.tf
@@ -0,0 +1,16 @@
+resource "local_file" "exemplo" {
+    filename = "exemplo.txt"
+    content = var.conteudo
+}
+
+variable "conteudo" {
+    type = string
+}
+
+output "id-do-arquivo" {
+    value = resource.local_file.exemplo.id
+}
+
+output "conteudo" {
+    value = var.conteudo
+}
